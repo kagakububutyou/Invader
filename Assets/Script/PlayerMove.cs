de1bc94ffdf0c6038db1d73c17f2e1afbc5881c2
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2015-02-05 16:09:22 +0900

    自機が動いた

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float Speed = 5.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        var value = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * value * Speed * Time.deltaTime);
+        
+    }
+}
